eab1d8f93d0bbd1f358245d0e138b3f76a482b73
lyricalsoul <lyricalsoul@proton.me>
2026-06-30 03:22:34 -0400

    fix: remove unused var, add todo

--- Scrobblr/Constants.swift
+++ Scrobblr/Constants.swift
@@ -7,11 +7,10 @@
 
 import CoreFoundation
 
+// TODO: add - single suffix for albums
 struct Constants {
     static let lfmApiKey = "573c000d85b8fd294e09fbf4bcc50bf0"
     static let lfmApiSecret = "a2eaf6c578c8d0bfda2c4846205aad0c"
-    
-    static let imageDimensionOnMacOS: CGFloat = 400
 
     static let trackNoiseKeywords = [
         "remaster(?:ed)?",
